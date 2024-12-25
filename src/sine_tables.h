8ec1b8b9fbb5ad32f296b203f420cd61017ac1c6
Clyde Shaffer <clydeshaffer@gmail.com>
2024-12-24 23:22:02 -0500

    sine tables

---
+++ src/sine_tables.h
@@ -0,0 +1,9 @@
+extern const char sineRadius8[128];
+
+extern const char sineRadius11[128];
+
+extern const char sineRadius16[128];
+
+extern const char sineRadius18[128];
+
+extern const char sineRadius23[128];
