59ea2832b88e02e3c885d93f7c37e63604628395
Clyde Shaffer <clydeshaffer@gmail.com>
2024-12-26 16:59:56 -0500

    add animations

--- src/main.c
+++ src/main.c
@@ -10,21 +10,25 @@
 
 #include "gen/assets/music.h"
 #include "gen/assets/bg.h"
+#include "gen/assets/bg/player.json.h"
 
 SpriteSlot bgImg;
+SpriteSlot playerImg;
 
 #define ROTATION_ANGLE 32
 char rotation_direction = 0;
 char rotation_timer = 0;
 
-#define PLAYER_NORMAL_Y 103
-#define PLAYER_SPRITE_X 33
-#define PLAYER_SPRITE_Y 103
+#define PLAYER_NORMAL_Y 111
+#define PLAYER_SUBFRAMES 6
 
 char player_x;
 signed char player_y;
 char player_vy;
 char target_x;
+char player_frame, player_frame_start, player_frame_end;
+char player_frame_end_next;
+char player_subframe;
 
 char win_state;
 char prev_win_state;
@@ -36,6 +40,8 @@ char global_tick = 0;
 int main () {
     init_graphics();
     bgImg = allocate_sprite(&ASSET__bg__scene_bmp_load_list);
+    playerImg = allocate_sprite(&ASSET__bg__player_bmp_load_list);
+    set_sprite_frametable(playerImg, &ASSET__bg__player_json);
 
     grid_init(bgImg);
     push_rom_bank();
@@ -49,6 +55,11 @@ int main () {
     player_y = 0;
     player_vy = 0;
     target_x = GRID_CENTER_X;
+    player_frame = PLAYER_TAG_IDLE_START;
+    player_subframe = 0;
+    player_frame_start = PLAYER_TAG_IDLE_START;
+    player_frame_end = PLAYER_TAG_IDLE_END;
+    player_frame_end_next = player_frame_end;
 
     play_song(ASSET__music__katachidasu_mid, REPEAT_LOOP);
 
@@ -76,6 +87,10 @@ int main () {
             --rotation_timer;
             if(!rotation_timer) {
                 grid_rotation &= (32|64);
+                player_frame = PLAYER_TAG_IDLE_START;
+                player_frame_start = PLAYER_TAG_IDLE_START;
+                player_frame_end = PLAYER_TAG_IDLE_END;
+                player_frame_end_next = player_frame_end;
             }
         } else {
             rotation_direction = 0;
@@ -87,14 +102,29 @@ int main () {
             }
             if(rotation_direction) {
                 rotation_timer = ROTATION_ANGLE;
+                player_frame_start = PLAYER_TAG_ROTATE_START;
+                player_frame_end_next = PLAYER_TAG_ROTATE_END;
+                player_frame_end = PLAYER_TAG_ROTATE_END;
+                player_frame = PLAYER_TAG_ROTATE_START;
+                player_subframe = 0;
             }
         }
 
         if(player1_new_buttons & INPUT_MASK_LEFT) {
             target_x -= 8;
+            player_frame_start = PLAYER_TAG_IDLE_START;
+            player_frame_end_next = PLAYER_TAG_IDLE_END;
+            player_frame_end = PLAYER_TAG_STEP_RIGHT_END;
+            player_frame = PLAYER_TAG_STEP_RIGHT_START+2;
+            player_subframe = 0;
         }
         if(player1_new_buttons & INPUT_MASK_RIGHT) {
             target_x += 8;
+            player_frame_start = PLAYER_TAG_IDLE_START;
+            player_frame_end_next = PLAYER_TAG_IDLE_END;
+            player_frame_end = PLAYER_TAG_STEP_RIGHT_END;
+            player_frame = PLAYER_TAG_STEP_RIGHT_START+2;
+            player_subframe = 0;
         }
 
         if(target_x < (GRID_CENTER_X - 16)) {
@@ -106,6 +136,11 @@ int main () {
         if(target_x == player_x) {
             if(player1_new_buttons & INPUT_MASK_C) {
                 grid_send_bullet(player_x);
+                player_frame_start = PLAYER_TAG_IDLE_START;
+                player_frame_end_next = PLAYER_TAG_IDLE_END;
+                player_frame_end = PLAYER_TAG_BULLET_END;
+                player_frame = PLAYER_TAG_BULLET_START+2;
+                player_subframe = 0;
             }
         }
         if(target_x < player_x) player_x -= 2;
@@ -121,7 +156,16 @@ int main () {
             win_state = 0;
         }
 
-        queue_draw_sprite(player_x - 8, PLAYER_NORMAL_Y + (player_y >> 2), 16, 16, PLAYER_SPRITE_X, PLAYER_SPRITE_Y, bgImg);
+        //queue_draw_sprite(player_x - 8, PLAYER_NORMAL_Y + (player_y >> 2), 16, 16, PLAYER_SPRITE_X, PLAYER_SPRITE_Y, bgImg);
+        queue_draw_sprite_frame(playerImg, player_x, PLAYER_NORMAL_Y + (player_y >> 2), player_frame, 0);
+        if((++player_subframe) == PLAYER_SUBFRAMES) {
+            player_subframe = 0;
+            if((++player_frame) == player_frame_end) {
+                player_frame_end = player_frame_end_next;
+                player_frame = player_frame_start;
+            }
+        }
+        
 
         if(win_state & GRID_DRAW_RESULT_WIN) {
             //queue_draw_box(65,33, 16, 16, 251);
